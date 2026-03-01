7b3cfdd945da06d882cd8a7cee5f3e114a5dd45d
coehlrich <coehlrich@users.noreply.github.com>
2026-03-02 02:50:47 +1300

    Readd support for client and server along with a bunch of other things

--- plugin/src/main/java/net/neoforged/neoform/tasks/PrepareJarForDecompiler.java
+++ plugin/src/main/java/net/neoforged/neoform/tasks/PrepareJarForDecompiler.java
@@ -2,17 +2,21 @@ package net.neoforged.neoform.tasks;
 
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class PrepareJarForDecompiler extends ToolAction {
     @InputFile
+    @Optional
     public abstract RegularFileProperty getClient();
 
     @InputFile
+    @Optional
     public abstract RegularFileProperty getServer();
 
     @OutputFile
@@ -20,14 +24,18 @@ public abstract class PrepareJarForDecompiler extends ToolAction {
 
     @TaskAction
     public void execute() throws IOException {
-        var clientJar = getClient().getAsFile().get();
-        var serverJar = getServer().getAsFile().get();
-        var joinedJar = getOutput().getAsFile().get();
+        Map<String, String> parameters = new HashMap<>();
 
-        exec(Map.of(
-                "inputClientJar", clientJar.getAbsolutePath(),
-                "inputServerJar", serverJar.getAbsolutePath(),
-                "output", joinedJar.getAbsolutePath()
-        ));
+        if (getClient().isPresent()) {
+            parameters.put("inputClientJar", getClient().getAsFile().get().getAbsolutePath());
+        }
+
+        if (getServer().isPresent()) {
+            parameters.put("inputServerJar", getServer().getAsFile().get().getAbsolutePath());
+        }
+
+        parameters.put("output", getOutput().getAsFile().get().getAbsolutePath());
+
+        exec(parameters);
     }
 }
